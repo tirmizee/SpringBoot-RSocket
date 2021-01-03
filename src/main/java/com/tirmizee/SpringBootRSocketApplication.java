e3b11bad24810923b15f9c8945a51b5d3d3adb21
Pratya Yeekhaday <pratya.y@gdl-technology.com>
2021-01-03 11:25:28 +0700

    init project

---
+++ src/main/java/com/tirmizee/SpringBootRSocketApplication.java
@@ -0,0 +1,13 @@
+package com.tirmizee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRSocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRSocketApplication.class, args);
+	}
+
+}
